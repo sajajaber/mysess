cb541b9d79fe3bbc1425415eb4211f1827fe5042
Saja Jaber <sajajaber3412@gmail.com>
2026-05-13 13:03:37 +0300

    Database connection: config/database

---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php
+/* Database configuration, this allows us to store the database information and connect to MySQL database
+We initialize it instead of using mysqli_connect() every time we want to connect to the database.
+*/
+class Database{ 
+  private $host = "localhost"; // database server, currently we are using local server
+  private $username = "root"; 
+  private $password = "";
+  private $db_name = "db_mysess";
+  private $charset = "utf8mb4";
+
+  // create the PDO connection and return it. PDO is a more secure and flexible way to connect to the database compared to mysqli
+  protected function connect() {
+
+    try {
+      $dsn = "mysql:host=$this->host;dbname=$this->db_name;charset=$this->charset";
+      $pdo = new PDO($dsn, $this->username, $this->password);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+      return $pdo;
+    } catch (PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
+  }
+}
+?>
---
+++ core/Session.php
@@ -0,0 +1,15 @@
+<?php
+/* This class is responsible for:
+    starting sessions
+    storing logged-in user data
+    checking if user is logged in
+    logout users */
+
+    class Session {
+      /* Our session methods  do not depend on other object properties, that's why we define them as static methods */
+       
+    }
+
+
+
+?>
